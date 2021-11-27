69070c30b4202de90d4bd2585d1ef544cf7fed4e
vbyrnes <BYRNESVA@GMAIL.COM>
2021-11-26 21:45:45 -0500

    Update Scanner.py

--- Scanner.py
+++ Scanner.py
@@ -16,17 +16,33 @@ resp = input("""\nPlease enter the type of scan you want to run
                 2)UDP Scan
                 3)Comprehensive Scan \n""")
 print("You have selected option: ", resp)
-resp_dict={'1':['-v -sS','tcp'],'2':['-v -sU','udp'],'3':['-v -sS -sV -sC -A -O','tcp']}
-if resp not in resp_dict.keys():
-    print("enter a valid option")
-else:
-    print("nmap version: "(scanner.nmap_version()))
-    scanner.scan(ip_addr,"1-1024",resp_dict[resp][0]) #the # are port range to scan, the last part is the scan type
+resp=int(resp)
+if resp==1:
+    print("nmap version: ", scanner.nmap_version())
+    scanner.scan(ip_addr,'1-1024','-v -sS') #port range to scan, the last part is the scan type
     print(scanner.scaninfo())
-    if scanner.scaninfo()=='up':
-        print("Scanner Status: ",scanner[ip_addr].state())
-        print(scanner[ip_addr].all_protocols())
-        print("Open Ports: ",scanner[ip_addr][resp_dict[resp][1]].keys())  #display all open ports
+    print("Scanner Status: ",scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ",scanner[ip_addr]['tcp'].keys())
+elif resp==2:
+    print("nmap version: ", scanner.nmap_version())
+    scanner.scan(ip_addr,'1-1024','-v -sU') #port range to scan, the last part is the scan type
+    print(scanner.scaninfo())
+    print("Scanner Status: ",scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ",scanner[ip_addr]['tcp'].keys())
+elif resp==3:
+    print("nmap version: ", scanner.nmap_version())
+    scanner.scan(ip_addr,'1-1024','-v -sS -sV -sC -A -O') #port range to scan, the last part is the scan type
+    print(scanner.scaninfo())
+    print("Scanner Status: ",scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ",scanner[ip_addr]['tcp'].keys())
+else:
+    print("Ivalid option. Please try again.")
+    exit
+    
+    
 
 
 
